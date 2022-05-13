468addfd870f84600e48a4f43be8a245da0cf8a2
maba-dev <cherif.ba@hotmail.fr>
2022-05-13 09:25:40 +0000

    task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Start Flask web App"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Display Hello HBNB! """
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """Display HBNB """
+    return 'HBNB'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
