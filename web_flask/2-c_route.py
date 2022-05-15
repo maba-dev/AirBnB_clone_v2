ada399ac06d44e43fb8897f9469f42388fd7fb59
maba-dev <cherif.ba@hotmail.fr>
2022-05-15 13:13:28 +0000

    correct betty

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -10,14 +10,18 @@ def hello():
     """Display Hello HBNB! """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hello_hbnb():
     """Display HBNB """
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def variable(text):
-    return 'C %s' % text.replace("_", " ") 
+    """Display a variable"""
+    return 'C %s' % text.replace("_", " ")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
