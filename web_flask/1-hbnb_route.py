51d07ac39fc1320102c3aea9bec39bee0dc19413
maba-dev <cherif.ba@hotmail.fr>
2022-05-15 13:11:35 +0000

    correct betty

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,10 +10,12 @@ def hello():
     """Display Hello HBNB! """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hello_hbnb():
     """Display HBNB """
     return 'HBNB'
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
