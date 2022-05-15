57b85bdd218ee5c665df564b6a74cc11de60b5a3
maba-dev <cherif.ba@hotmail.fr>
2022-05-15 15:27:31 +0000

    display “n is a number” only if n is an integer

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Start Flask web App"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Display Hello HBNB! """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """Display HBNB """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def variable(text):
+    """Display a variable"""
+    return 'C %s' % text
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def variable_python(text='is cool'):
+    """Display a variable"""
+    return 'Python %s' % text.replace("_", " ")
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    """ display “n is a number” only if n is an integer"""
+    return '%d is a number' % n
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
